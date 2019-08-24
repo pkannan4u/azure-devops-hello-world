704221c9220f61b1136d53238130ccbb397294de
Frank Caron <frank@frankcaron.com>
2019-08-24 15:42:47 -0400

    Adjusted app.js for Azure

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 1337;
+
+console.log("Server running at http://localhost:%d", port);
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
